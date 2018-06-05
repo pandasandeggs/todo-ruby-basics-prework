aa19365ede8de6a795c2d12824841d3f9a7bc991
Chantal Wallace <theteatreehouse@gmail.com>
2018-06-05 00:46:36 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,17 +1,17 @@
 def division(num1, num2)
- 
+ return num1 / num2
 end
 
-def assign_variable(value)
-  
+def assign_variable(name)
+  returns name
 end
 
-def argue
-  
+def argue(ment)
+  ment
 end
 
 def greeting
-
+  
 end
 
 def return_a_value
